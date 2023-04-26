7745bd62b1febd3327a92c2d71eecfa62c7d0a76
jojomutanu003 <musalumutanu@gmail.com>
2023-04-26 18:03:37 +0300

    routes change

--- src/components/ussd-integration/ussd.controller.ts
+++ src/components/ussd-integration/ussd.controller.ts
@@ -11,11 +11,27 @@ class UssdController {
   getReqBody(@Body() dto: UssdBody) {
     console.log(dto);
     if (dto.text == '') {
-        return ' CON Would you Like to report an Incident \n 1.FGM \n 2.Rape';
-    } else if (dto.text == 'FGM' || '1' || 'fgm' || 'Fgm') {
-      return 'CON FGM landing'
-    } else if (dto.text == 'Rape' || '2' || 'rape') {
-      return 'CON Rape landing'
+        return 'CON Who are reporting for?/ Unaripoti kwa niaba ya nani? \n 1. Self/ Wewe mwenyewe \n 2. Someone else/ Mtu mwengine'
+    } else if (dto.text == '1') {
+        return ' CON Sasa, unaitwa nani?/ Hi, what\'s your name \n Name: Your Name \n Jina: Jina Lako';
+    } 
+    else if (dto.text.includes('+254')) {
+        return `END  Tunakujali na tunataka kukusaidia uwe salama! `
+    }
+    else if (dto.text.includes('*3*')) {
+        return `CON  Tupige Simu wapi \n Anzia na +254 `
+      }
+    else if (dto.text.includes('*3')) {
+       return `CON ${dto.text.slice(2, -2)} Ningependa kujua mahali ulipo(jina LA shule,jina LA kanisa/jina LA hospitali)/what is your location?(name of town/name of school/name of nearest hospital`
+      }
+    else if (dto.text.includes('1*')){
+
+        return `CON Jambo ${dto.text.slice(2)}  Jambo Gani Limetendeka 
+             3. Sexual Assault/ Ubakaji 
+             4. FGM/ Ukeketaji wa sehemu zangu za sir`
+    }
+     else if (dto.text == '2*'+ '[a-zA-Z]+') {
+      return 'CON we are good';
     } else {
       return ' END Invalid input';
     }
@@ -26,3 +42,4 @@ class UssdController {
   }
 }
 export default UssdController;
+
